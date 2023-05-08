bc1dd932ef453651e399958cc34dd39dc228ffc1
Maximiliano Luna <maxiespn21@gmail.com>
2023-05-08 15:14:01 -0300

    Se implementó la sección nuestro hogar con carrusel de imágenes. Usando Glider.js

---
+++ js/carrusel.js
@@ -0,0 +1,32 @@
+window.addEventListener("load", function () {
+  new Glider(document.querySelector(".carrusel__lista"), {
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    draggable: true,
+    dots: ".carrusel__indicadores",
+    arrows: {
+      prev: ".carrusel__anterior",
+      next: ".carrusel__siguiente",
+    },
+    responsive: [
+      {
+        // screens greater than >= 580px
+        breakpoint: 580,
+        settings: {
+          // Set to `auto` and provide item width to adjust to viewport
+          slidesToShow: 2,
+          slidesToScroll: 2,
+        },
+      },
+      {
+        // screens greater than >= 991px
+        breakpoint: 991,
+        settings: {
+          slidesToShow: 4,
+          slidesToScroll: 4,
+          draggable: false,
+        },
+      },
+    ]
+  });
+});
--- js/custom.js
+++ js/custom.js
@@ -1,33 +1,32 @@
 
-  (function ($) {
-  
+(function ($) {
   "use strict";
 
-    // COUNTER NUMBERS
-    jQuery('.counter-thumb').appear(function() {
-      jQuery('.counter-number').countTo();
-    });
-    
-    // CUSTOM LINK
-    $('.smoothscroll').click(function(){
-    var el = $(this).attr('href');
-    var elWrapped = $(el);
-    var header_height = $('.navbar').height();
+  // COUNTER NUMBERS
+  jQuery(".counter-thumb").appear(function () {
+    jQuery(".counter-number").countTo();
+  });
 
-    scrollToDiv(elWrapped,header_height);
+  // CUSTOM LINK
+  $(".smoothscroll").click(function () {
+    var el = $(this).attr("href");
+    var elWrapped = $(el);
+    var header_height = $(".navbar").height();
+
+    scrollToDiv(elWrapped, header_height);
     return false;
 
-    function scrollToDiv(element,navheight){
+    function scrollToDiv(element, navheight) {
       var offset = element.offset();
       var offsetTop = offset.top;
-      var totalScroll = offsetTop-navheight;
+      var totalScroll = offsetTop - navheight;
 
-      $('body,html').animate({
-      scrollTop: totalScroll
-      }, 300);
+      $("body,html").animate(
+        {
+          scrollTop: totalScroll,
+        },
+        300
+      );
     }
-});
-    
-  })(window.jQuery);
-
-
+  });
+})(window.jQuery);
