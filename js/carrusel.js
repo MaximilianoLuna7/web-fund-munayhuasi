65f81738d42f893d6ac6284e914056fd41be680f
Maximiliano Luna <maxiespn21@gmail.com>
2023-05-21 11:37:05 -0300

    Corrección del carrusel para usar con el dedo

--- js/carrusel.js
+++ js/carrusel.js
@@ -24,7 +24,6 @@ window.addEventListener("load", function () {
         settings: {
           slidesToShow: 4,
           slidesToScroll: 4,
-          draggable: false,
         },
       },
     ]
